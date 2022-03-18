f92c069dd8d2d801de57d30ca829598fa6ae195b
uk1996 <csuk1947@gmail.com>
2022-03-18 15:29:17 +0900

    update Django_example

--- Django_example/instagram/admin.py
+++ Django_example/instagram/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
+from django.utils.safestring import mark_safe
 
 
 #admin.site.register(Post)
@@ -10,10 +11,15 @@ from .models import Post
 
 @admin.register(Post) # Wrapping
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'message','message_length', 'is_public', 'created_at', 'updated_at']
+    list_display = ['id', 'photo_tag', 'message','message_length', 'is_public', 'created_at', 'updated_at']
     list_display_links = ['message']
     list_filter = ['created_at', 'is_public']
     search_fields = ['id', 'message']
+
+    def photo_tag(self, post):
+        if post.photo:
+           return mark_safe(f"<img src = '{post.photo.url}' style='width: 75px;' />")
+        return None
 
     # def message_length(self, post): 
     #     return len(post.message)
--- Django_example/instagram/models.py
+++ Django_example/instagram/models.py
@@ -1,8 +1,27 @@
+from distutils import extension
 from django.db import models
+import os
+from uuid import uuid4
+from django.utils import timezone
+
+def uuid_name_upload_to(instance, filename):
+        app_label = instance.__class__._meta.app_label
+        cls_name = instance.__class__.__name__.lower()
+        ymd_path = timezone.now().strftime('%Y/%m/%d')
+        uuid_name = uuid4().hex
+        extension = os.path.splitext(filename)[-1].lower()
+        return '/'.join([
+            app_label,
+            cls_name,
+            ymd_path,
+            uuid_name[:2],
+            uuid_name + extension,
+        ])
 
 # Create your models here.
 class Post(models.Model):
     message = models.TextField()
+    photo = models.ImageField(blank=True, upload_to=uuid_name_upload_to)
     is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='생성시간')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='수정시간')
@@ -13,4 +32,6 @@ class Post(models.Model):
     
     def message_length(self): 
         return len(self.message)
-    message_length.short_description = '메시지 글자수'
+    message_length.short_description = '메세지 글자수'
+
+
