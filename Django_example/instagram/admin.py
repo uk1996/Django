c02b84d9e9526ebfb47b3c9030a4f0501102f39f
uk1996 <csuk1947@gmail.com>
2022-03-18 11:07:14 +0900

    update Django_example

--- Django_example/instagram/admin.py
+++ Django_example/instagram/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+#admin.site.register(Post)
+
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+# admin.site.register(Post, PostAdmin)
+
+@admin.register(Post) # Wrapping
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message','message_length', 'is_public', 'created_at', 'updated_at']
+    list_display_links = ['message']
+    list_filter = ['created_at', 'is_public']
+    search_fields = ['id', 'message']
+
+    # def message_length(self, post): 
+    #     return len(post.message)
+    # message_length.short_description = '메시지 글자수'
--- Django_example/instagram/models.py
+++ Django_example/instagram/models.py
@@ -3,5 +3,14 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    is_public = models.BooleanField(default=False, verbose_name='공개여부')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='생성시간')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정시간')
+
+    def __str__(self):
+        #return f"Custom Post object ({self.id})"
+        return self.message
+    
+    def message_length(self): 
+        return len(self.message)
+    message_length.short_description = '메시지 글자수'
