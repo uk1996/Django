50324531d033a3415d5b50890c5af8590994390d
uk1996 <csuk1947@gmail.com>
2022-03-23 09:29:22 +0900

    blog1 삭제

--- Django_example/blog1/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-
-# Create your views here.
-def post_list(requset):
-    qs = Post.objects.all()
-    return render(requset, 'blog1/post_list.html', {
-        'post_list':qs,
-    })
