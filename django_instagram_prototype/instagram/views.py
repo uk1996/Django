1d51b8c5c05a44d862cb2db75262de92db091468
SW Choi <csuk1947@gmail.com>
2022-03-30 19:00:05 +0900

    instagram/post_list 수정

--- django_instagram_prototype/instagram/views.py
+++ django_instagram_prototype/instagram/views.py
@@ -1,23 +1,31 @@
 from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404 # 템플릿 응답을 위한 shortcut 함수
 from .models import Post
-from django.views.generic import DetailView, ArchiveIndexView
+from django.views.generic import DetailView, ArchiveIndexView, ListView
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-@login_required
-def post_list(request:HttpRequest):
-    qs = Post.objects.all()
-    q = request.GET.get('q', '')
-    page = request.GET.get('page', '1')
-    if q:
-        qs = qs.filter(message__icontains=q)
-    paginator = Paginator(qs, 10) # 한 페이지당 10개씩
-    page_obj = paginator.get_page(page)
-    return render(request, 'instagram/post_list.html', {
-        'post_list':page_obj,
-        'q':q,
-    })
+# @login_required
+# def post_list(request:HttpRequest):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q', '')
+#     page = request.GET.get('page', '1')
+#     if q:
+#         qs = qs.filter(message__icontains=q)
+#     paginator = Paginator(qs, 10) # 한 페이지당 10개씩
+#     page_obj = paginator.get_page(page)
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list':page_obj,
+#         'q':q,
+#     })
+
+@method_decorator(login_required, name='dispatch')
+class PostListView(ListView):
+    model = Post
+    paginate_by = 10
+    
+post_list = PostListView.as_view()
 
 def post_detail(requset:HttpRequest, pk) -> HttpResponse: 
     # try:
