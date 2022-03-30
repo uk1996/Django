b8eb143420a09f0280c7d138a296df2dec9893f9
uk1996 <csuk1947@gmail.com>
2022-03-30 16:29:18 +0900

    post_list 페이징 기능 구현

--- django_instagram_prototype/instagram/views.py
+++ django_instagram_prototype/instagram/views.py
@@ -2,14 +2,18 @@ from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404 # 템플릿 응답을 위한 shortcut 함수
 from .models import Post
 from django.views.generic import DetailView
+from django.core.paginator import Paginator
 
 def post_list(request:HttpRequest):
     qs = Post.objects.all()
     q = request.GET.get('q', '')
+    page = request.GET.get('page', '1')
     if q:
         qs = qs.filter(message__icontains=q)
+    paginator = Paginator(qs, 10) # 한 페이지당 10개씩
+    page_obj = paginator.get_page(page)
     return render(request, 'instagram/post_list.html', {
-        'post_list':qs,
+        'post_list':page_obj,
         'q':q,
     })
 
