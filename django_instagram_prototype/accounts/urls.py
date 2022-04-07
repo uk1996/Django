c2920e7d5e82a7b62bcd96446111358bebabb411
uk1996 <csuk1947@gmail.com>
2022-04-07 13:34:19 +0900

    로그인 기능 구현

--- django_instagram_prototype/accounts/urls.py
+++ django_instagram_prototype/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
-    
+    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login')
 ]
